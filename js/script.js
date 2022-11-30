49c607a9230a6fc367f5a8ab95371499f213a076
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-11-30 15:53:01 +0100

    Modified script

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 
 const button = document.querySelector("button");
 button.addEventListener("click" , function(){
-    //AGGIUGNO la classe d-none per far scomparire il bottone, in quanto il gioco è avviato
-    button.classList.add("d-none");
-
-    //CREO il divParent che conterra le piccole caselle
+    
+    //NASCONDO il bottone dopo il click
+    //button.classList.add("d-none");
+    
     const mainElement = document.querySelector("main");
+    //SVUOTO l'elemento main Element, resetto
+    mainElement.innerHTML="";
+    //CREO il divParent che conterra le piccole caselle
     const newParentDivElement = document.createElement("div");
     newParentDivElement.classList.add("m-auto");
     mainElement.append(newParentDivElement);
 
     //CICLO e creo i vari div che comporranno il divParent
     for(let i=1; i<=100; i++){
-        const divElement = document.querySelector("div");
-        divElement.classList.add("flex-wrap" , "d-flex");
+        newParentDivElement.classList.add("flex-wrap" , "d-flex");
         const newDivElement = document.createElement("div");
 
         //AGGIUNGO evento al click su un divElement
@@ -24,7 +26,7 @@ button.addEventListener("click" , function(){
         //INSERISCO nell'innerHTML del divChild i numeri del contatore e le classi
         newDivElement.innerHTML = "<span>" + i + "</span>"
         newDivElement.classList.add("box", "d-flex" , "justify-content-center", "align-items-center" , "fs-3");
-        divElement.append(newDivElement);
+        newParentDivElement.append(newDivElement);
     }
 });
 
@@ -33,11 +35,10 @@ button.addEventListener("click" , function(){
 
 //myGetElementById("output", "fw-bold" , "ciao");
 
-/*FUNCTION
+
 function myGetElementById(elementId, elementClass, elementInner){
     const elementName = document.getElementById(elementId);
     elementName.classList.add(elementClass);
     elementName.innerHTML = elementInner;
 
 }
-*/
